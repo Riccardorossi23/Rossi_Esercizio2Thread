29e5e9dd51282563cf32cbd7ebbd88eaabe3efd1
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2021-02-05 09:11:17 +0100

    Add project files.

---
+++ Rossi_Esercizio2Thread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rossi_Esercizio2Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("numero corridori: ");
+            int n = int.Parse(Console.ReadLine());
+            List<string> partecipanti = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"come si chiama {i}:");
+                string name = Console.ReadLine();
+                partecipanti.Add(name);
+            }
+            for(int i = 1; i <= n; i++)
+            {
+                Thread thread = new Thread(() => corsa(partecipanti[i]));
+                thread.Start();
+
+            }
+            Console.ReadLine();
+
+        }
+        private static void corsa(string name)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine($"il corridore {name} ha percorso {i} ");
+            }
+            Console.WriteLine($"la gara {name}");
+
+        }
+
+    }
+}
